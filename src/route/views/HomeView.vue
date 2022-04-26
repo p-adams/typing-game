bf5ca05715acd8a61918a266a78c80b4521196f2
Paul Adams <ghusiyy@gmail.com>
2022-04-25 23:19:16 -0700

    init game variables

--- src/route/views/HomeView.vue
+++ src/route/views/HomeView.vue
@@ -2,12 +2,21 @@
 import TextPrompt, { promptText } from "../../components/TextPrompt.vue";
 import GameScreen from "../../components/GameScreen.vue";
 import TextInput from "../../components/TextInput.vue";
-
+import { ref } from "vue";
+const TIME_LIMIT = 30_000;
+const wordCount = ref(0);
+const wordsPerMinute = ref(0);
+const draft = ref("");
 // track typing progress
 function track(e) {
-  const wordMatch = promptText.match(e.target.value);
+  draft.value = e.target.value;
+  const wordMatch = promptText.match(draft.value);
   if (wordMatch) {
+    // compare draft with promptText
+    // increment wordCount
     // add word count to running WPM tally
+  } else {
+    console.log("no match");
   }
 }
 </script>
